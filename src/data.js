4c6bf979f716fafaf5d9391b9926cf4d0f3a8c37
Mufaddal <muffi606@gmail.com>
2021-01-21 18:08:03 +0500

    first

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export default {
+	products: [
+	{
+		name:  'Slim Shirt',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 60,
+		brand: 'Nike',
+		rating:4.5,
+		numReviews: 10
+	},
+	{
+		name:  'Fit Shirt',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 60,
+		brand: 'Nike',
+		rating:4.5,
+		numReviews: 10
+	},
+	{
+		name:  'Slim Shirt',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 600,
+		brand: 'Nike',
+		rating:4.2,
+		numReviews: 5
+	},
+	{
+		name:  'Best Pants',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 670,
+		brand: 'Nike',
+		rating:4.5,
+		numReviews: 10
+	},
+	{
+		name:  'Dressing Shirt',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 680,
+		brand: 'Nike',
+		rating:4.5,
+		numReviews: 10
+	},
+	{
+		name:  'Slim Shirt',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 960,
+		brand: 'Nike',
+		rating:4.5,
+		numReviews: 10
+	},
+	{
+		name:  'Slim Shirt',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 610,
+		brand: 'Nike',
+		rating:4.5,
+		numReviews: 10
+	},
+	]
+}
