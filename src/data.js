58ead5a47f2e8e0248989f50cfb8ec887626773f
Mufaddal Shakir <muffi606@gmail.com>
2021-01-23 09:26:24 +0500

    Add files via upload
    1:30:52

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default {
+	products: [
+	{
+		_id: '001',
+		name:  'Organic Gur (500 Grams)',
+		category: 'Natural Sugarcane',
+		image: '/images/d1.jpg',
+		price: 100,
+		brand: 'Pakistan',
+		rating:4.5,
+        numReviews: 10,
+        description: 'Organic Gur has been bought from the farms of Mardan'
+	},
+	{	
+		_id: '002',
+		name:  'Brown Sugar (500 Grams)',
+		category: 'Natural Sugarcane',
+		image: '/images/d1.jpg',
+		price: 120,
+		brand: 'Pakistan',
+		rating:4.5,
+		numReviews: 10
+	},
+	{
+		_id: '003',
+		name:  'Slim Shirt',
+		category: 'Shirts',
+		image: '/images/d1.jpg',
+		price: 610,
+		brand: 'Nike',
+		rating:4.5,
+		numReviews: 10
+	},
+	]
+}
